39a5e771cbcd837d343776fcafd8a33a731ae4c7
Midi <18530201078@163.com>
2019-03-17 22:20:42 +0800

    housailei

--- src/view/project.vue
+++ src/view/project.vue
@@ -1,15 +1,109 @@
 <template>
   <div>
-    111
+    <div id="u165_div">
+      <img id="u165" src="../assets/imgs/u165.jpg">
+      <div id="u166_div">
+        <p id="u200" class="center content">项目规划</p>
+        <img src="../assets/imgs/u50.png">
+        <h2 id="u202" class="center">PROJECT INTRODUCTION</h2>
+        <p
+          id="u201"
+        >亦城时代广场由北京亦庄盛元投资开发有限公司开发，总建筑面积约43万㎡，由12栋建筑组成的上午甲级写字楼聚落，项目位于北京经济开发区路东区，地处经海路与科创路五街主干道交叉口，毗邻在建的地铁17号线北神树站。</p>
+      </div>
+    </div>
+    <p id="u168" class="center">项目规划</p>
+    <img src="../assets/imgs/u50.png">
+    <p id="u170" class="center">PROJECT PLANNING</p>
+    <p id="u171" class="center content">亦城时代广场共计12栋商务写字楼，7栋临街商铺组成，一期产品有T5、T6、T7、T8、T9</p>
+    <p id="u173" class="center content">五栋写字楼和B3、B4、B5、B6四栋临街商铺完成。</p>
+    <div id="u172_div">
+      <img id="u172" src="../assets/imgs/u172.png">
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
 <style scoped lang="scss">
+img {
+  display: block;
+  margin: 0 auto;
+}
+.content {
+  font-size: 18px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  p {
+    width: 1180px;
+    color: #666;
+    text-align: center;
+  }
+}
 
+.center {
+  text-align: center;
+}
+#u165_div {
+  width: 100%;
+  height: 820px;
+  margin: 0 auto;
+}
+#u165 {
+  width: 100%;
+}
+#u166_div {
+  margin-left: 1150px;
+  margin-top: -600px;
+  position: absolute;
+  width: 616px;
+  height: 420px;
+  background-color: rgba(0, 0, 0, 0.458823529411765);
+}
+#u168 {
+  font-size: 32px;
+  margin-top: 80px;
+}
+#u170 {
+  font-size: 24px;
+  color: #b28955;
+}
+#u171 {
+  margin-top: 40px;
+  font-size: 18px;
+  color: #666666;
+}
+#u172 {
+  margin-top: 40px;
+  width: 1300px;
+}
+#u172_div {
+  width: 1300px;
+  margin: 0 auto;
+}
+#u173 {
+  font-size: 18px;
+  color: #666666;
+}
+#u200 {
+  margin-top: 40px;
+  font-size: 36px;
+  color: #cc9e00;
+}
+#u201 {
+  width: 500px;
+  margin: 0 auto;
+  font-size: 24px;
+  color: #fff;
+  margin-top: 30px;
+}
+#u202 {
+  color: #b28955;
+  font-size: 24px;
+}
+.u260 {
+  display: block;
+}
 </style>
